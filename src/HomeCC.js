8348706ecfd24fe06236c5d6a7450beff2446d3a
Ruwanya Sumindi <ruwanyasumin@gmail.com>
2022-10-15 20:26:29 +0530

    Simple change

--- src/HomeCC.js
+++ src/HomeCC.js
@@ -7,7 +7,7 @@ class HomeCC extends React.Component {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <Text style={styles.red}>Ruwanya Sumindi</Text>
+        <Text style={styles.red}>My name is RuwanYA sUMINDI</Text>
 
         <View style={styles.box} />
         <View style={styles.box1} />
